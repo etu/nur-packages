c158831642a9c116b619476ea20c183b1ef63df0
Elis Hirwing <elis@hirwing.se>
2023-02-12 13:46:43 +0100

    flake: Add two firefox extensions

--- default.nix
+++ default.nix
@@ -5,4 +5,8 @@
   g90updatefw = pkgs.callPackage ./pkgs/g90updatefw {};
   llr = pkgs.callPackage ./pkgs/llr {};
   mkvcleaner = pkgs.callPackage ./pkgs/mkvcleaner {};
+
+  # Firefox extensions
+  firefox-extension-elasticvue = pkgs.callPackage ./pkgs/firefox-extensions/elasticvue.nix {};
+  firefox-extension-streetpass-for-mastodon = pkgs.callPackage ./pkgs/firefox-extensions/streetpass-for-mastodon.nix {};
 }
