5d8206fd5b0663596c675e4156dde510626499bf
Elis Hirwing <elis@hirwing.se>
2023-02-12 10:00:34 +0100

    flake: Add package `font-etuvetica`

--- default.nix
+++ default.nix
@@ -1,4 +1,5 @@
 {pkgs ? import <nixpkgs> {}, ...}: {
+  font-etuvetica = pkgs.callPackage ./pkgs/font-etuvetica {};
   g90updatefw = pkgs.callPackage ./pkgs/g90updatefw {};
   llr = pkgs.callPackage ./pkgs/llr {};
   mkvcleaner = pkgs.callPackage ./pkgs/mkvcleaner {};
