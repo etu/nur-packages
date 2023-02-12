c9fc5899430d7bd1000244e1336473a7176788b1
Elis Hirwing <elis@hirwing.se>
2023-02-12 10:38:28 +0100

    flake: Add package `chalet`

--- default.nix
+++ default.nix
@@ -1,4 +1,5 @@
 {pkgs ? import <nixpkgs> {}, ...}: {
+  chalet = pkgs.callPackage ./pkgs/chalet {};
   font-etuvetica = pkgs.callPackage ./pkgs/font-etuvetica {};
   font-talyznewroman = pkgs.callPackage ./pkgs/font-talyznewroman {};
   g90updatefw = pkgs.callPackage ./pkgs/g90updatefw {};
