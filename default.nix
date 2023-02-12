21ac016ad2f0129c5c635bb594a5a609c322e65e
Elis Hirwing <elis@hirwing.se>
2023-02-12 10:13:06 +0100

    flake: Add package `font-talyznewroman`

--- default.nix
+++ default.nix
@@ -1,5 +1,6 @@
 {pkgs ? import <nixpkgs> {}, ...}: {
   font-etuvetica = pkgs.callPackage ./pkgs/font-etuvetica {};
+  font-talyznewroman = pkgs.callPackage ./pkgs/font-talyznewroman {};
   g90updatefw = pkgs.callPackage ./pkgs/g90updatefw {};
   llr = pkgs.callPackage ./pkgs/llr {};
   mkvcleaner = pkgs.callPackage ./pkgs/mkvcleaner {};
