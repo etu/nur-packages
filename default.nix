3edae4f440efe26386f01e34ad44fdeff188d546
Elis Hirwing <elis@hirwing.se>
2023-02-12 09:30:37 +0100

    flake: Add package `g90updatefw`

--- default.nix
+++ default.nix
@@ -1,4 +1,5 @@
 {pkgs ? import <nixpkgs> {}, ...}: {
+  g90updatefw = pkgs.callPackage ./pkgs/g90updatefw {};
   llr = pkgs.callPackage ./pkgs/llr {};
   mkvcleaner = pkgs.callPackage ./pkgs/mkvcleaner {};
 }
